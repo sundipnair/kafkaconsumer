7cd4c526b236b7c489d3846d6e91733d92f6c601
P95048488 <P95048488@capita.co.uk>
2018-10-31 16:09:31 +0000

    threading logic

--- KafkaConsumer.py
+++ KafkaConsumer.py
@@ -7,50 +7,45 @@ import json
 from kafka import KafkaConsumer, KafkaProducer
 import mysql.connector
 
-# mydb = mysql.connector.connect(
-#   host="test-mysql",
-#   user="sundip",
-#   passwd="asdlkj12345",
-#   database="Candidate"
-# )
+mydb = mysql.connector.connect(
+  host="test-mysql",
+  user="sundip",
+  passwd="asdlkj12345",
+  database="Candidate"
+)
 
-class Consumer():
+class Consumer(threading.Thread):
 
-    #daemon = True
+    daemon = True
     def run(self):
-
-      print("this is print message")
-
-      # logging.debug("this is debug message")
-
-      # logging.info("this is info message")
-
-      # logging.warning("this is warning message")
-
-      # logging.error("this is error message")
-
-      logging.warning("this is warning message")
-
       consumer = KafkaConsumer(bootstrap_servers='confkafka-cp-kafka:9092',
                                 auto_offset_reset='earliest',
                                 value_deserializer=lambda m: json.loads(m.decode('utf-8')))
       consumer.subscribe(['candidate-topic'])
       for message in consumer:
-        #print(message)    
-        logging.warning("got message")    
         logging.warning(message)    
 
-            # mycursor = mydb.cursor()
+        mycursor = mydb.cursor()
 
-            # sql = "insert into BasicData (FirstName, LastName, Email) VALUES (%s, %s, %s)"
-            # val = ("TestF", "TestL", "TEstEmail1")
-            # mycursor.execute(sql, val)
+        sql = "insert into BasicData (FirstName, LastName, Email) VALUES (%s, %s, %s)"
+        val = ("TestF", "TestL", "TEstEmail1")
+        mycursor.execute(sql, val)
 
-            # mydb.commit()
+        mydb.commit()
 
-            # logging.debug(mycursor.rowcount, "record inserted.")
+        logging.warning(mycursor.rowcount, "record inserted.")
 
-c = Consumer()
-c.run()
-
-logging.warning("this is the last message")
+def main():
+    threads = [
+        Consumer()
+    ]
+    for t in threads:
+        t.start()
+    time.sleep(0.1)
+if __name__ == "__main__":
+    logging.basicConfig(
+        format='%(asctime)s.%(msecs)s:%(name)s:%(thread)d:' +
+               '%(levelname)s:%(process)d:%(message)s',
+        level=logging.INFO
+    )
+    main()
