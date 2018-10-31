60852157bfb8905936225ac9bea552e5e25b2ca2
P95048488 <P95048488@capita.co.uk>
2018-10-31 16:19:37 +0000

    comment out loggin config

--- KafkaConsumer.py
+++ KafkaConsumer.py
@@ -43,9 +43,9 @@ def main():
         t.start()
     time.sleep(0.1)
 if __name__ == "__main__":
-    logging.basicConfig(
-        format='%(asctime)s.%(msecs)s:%(name)s:%(thread)d:' +
-               '%(levelname)s:%(process)d:%(message)s',
-        level=logging.INFO
-    )
+    # logging.basicConfig(
+    #     format='%(asctime)s.%(msecs)s:%(name)s:%(thread)d:' +
+    #            '%(levelname)s:%(process)d:%(message)s',
+    #     level=logging.INFO
+    # )
     main()
