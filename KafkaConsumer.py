3d2cb349d570e3902c18f060af10e66cabfa49c2
P95048488 <P95048488@capita.co.uk>
2018-10-31 11:53:40 +0000

    add consumer code

--- KafkaConsumer.py
+++ KafkaConsumer.py
@@ -2,21 +2,37 @@ import threading
 import logging
 import time
 import json
+import logging
 from kafka import KafkaConsumer, KafkaProducer
 
-class Producer(threading.Thread):
+# class Producer(threading.Thread):
+#     daemon = True
+#     def run(self):
+#         producer = KafkaProducer(bootstrap_servers='confkafka-cp-kafka:9092',
+#                                  value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+
+#         producer.send('candidate-topic', {"Id":0,"FirstName":"Kafka1","LastName":"Prod1","Email":"prod1@kafka.com"})
+
+#         # while True:
+#         #     producer.send('my-topic', {"dataObjectID": "test1"})
+#         #     producer.send('my-topic', {"dataObjectID": "test2"})
+#         #     time.sleep(1)
+
+
+# prod = Producer()
+# prod.run()
+
+
+class Consumer(threading.Thread):
     daemon = True
     def run(self):
-        producer = KafkaProducer(bootstrap_servers='confkafka-cp-kafka:9092',
-                                 value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+        consumer = KafkaConsumer(bootstrap_servers='confkafka-cp-kafka:9092',
+                                 auto_offset_reset='earliest',
+                                 value_deserializer=lambda m: json.loads(m.decode('utf-8')))
+        consumer.subscribe(['candidate-topic'])
+        for message in consumer:
+            #print (message)
+            logging.error(message)    
 
-        producer.send('candidate-topic', {"Id":0,"FirstName":"Kafka1","LastName":"Prod1","Email":"prod1@kafka.com"})
-
-        # while True:
-        #     producer.send('my-topic', {"dataObjectID": "test1"})
-        #     producer.send('my-topic', {"dataObjectID": "test2"})
-        #     time.sleep(1)
-
-
-prod = Producer()
-prod.run()
+c = Consumer()
+c.run()
